295ed8dcb5c154a3acce86bfc7e9eb4cc1595bc9
Bob <bob.pyae@oom.com.sg>
2025-01-29 19:58:28 +0800

    Initial Express Setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+
+// Load env vars 
+dotenv.config({path:'./config/config.env'});
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+// Run the server
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on PORT http://localhost:${PORT}/`));
