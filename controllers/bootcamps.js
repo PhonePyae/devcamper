e958b2ce90f1965c83e21a0401e0661b57caa8a2
PhonePyae <phonepyaethantzin2000@gmail.com>
2025-02-25 20:23:58 +0800

    Adding Pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -44,10 +44,32 @@ exports.getBootcamps = asyncHandler(
             query = query.sort('-createdAt');
         }   
 
+        // Pagination
+        const page = parseInt(req.query.page, 10) || 1;
+        const limit = parseInt(req.query.limit, 10) || 25;
+        const startIndex = (page - 1) * limit; 
+        const endIndex = page * limit;
+        const total = await Bootcamp.countDocuments();
+
         // executing query
         const bootcamps = await query;
 
-        res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps});
+        // Pagination result
+        const pagination = {};
+        if(endIndex < total){
+            pagination.next = {
+                page: page + 1,
+                limit
+            }
+        }   
+        if(startIndex > 0){
+            pagination.prev = {
+                page: page - 1,
+                limit
+            }
+        }
+
+        res.status(200).json({ success: true, count: bootcamps.length, pagination, data: bootcamps});
     }
 );
 
