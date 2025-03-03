2efb63ee4c5b25416e05c7e51022a95fa608cf0d
PhonePyae <bob.pyae@oom.com.sg>
2025-03-03 23:13:11 +0800

    Update & Delete Course

--- routes/courses.js
+++ routes/courses.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const {getCourses} = require('../controllers/courses');
+const {getCourses, getCourse, addCourse} = require('../controllers/courses');
 const router = express.Router({ mergeParams: true }); // mergeParams is used to merge the url parameters from the parent router with the child router
 
-router.route('/').get(getCourses);
+router.route('/').get(getCourses).post(addCourse);
+router.route('/:id').get(getCourse);
 
 module.exports = router;
