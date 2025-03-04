1d9abfa87d56526484805abb509fa9e5b4275de7
Phone Pyae <phonepyaethantzin2000@gmail.com>
2025-03-05 00:10:24 +0800

    Sign & Get JSON Web Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,9 +9,12 @@ exports.register = asyncHandler(async (req,res,next) => {
     const {name, email, password, role} = req.body;
     
     //Create user
-    const user = User.create({
+    const user = await User.create({
         name, email, password, role
     });
 
-    res.status(200).json({success: true})
+    //Create toke
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({success: true, token: token})
 })
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -41,6 +42,14 @@ const UserSchema = new mongoose.Schema({
 UserSchema.pre('save', async function(next){
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-})
+    next();
+});
+
+//Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function () {
+    return jwt.sign({ id: this._id.toString() }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    });
+};
 
 module.exports = mongoose.model('User', UserSchema);
