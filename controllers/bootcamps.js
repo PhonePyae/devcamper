923f743324fa8b4a1d0f5313851d527ec6211735
Phone Pyae <phonepyaethantzin2000@gmail.com>
2025-03-08 10:36:32 +0800

    Adding a User To Bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -31,6 +31,17 @@ exports.getBootcamp = asyncHandler(
 //@access   Private
 exports.createBootcamp = asyncHandler(
     async(req, res, next) => {
+        //Add user to req.body
+        req.body.user = req.user.id;
+
+        //Check for published bootcamp
+        const publishedBootcamp = await Bootcamp.findOne({user: req.user.id});
+
+        //If user is not admin, they can only one bootcamp
+        if(publishedBootcamp && req.user.role !== 'admin'){
+            return next(new ErrorResponse(`User ID ${req.user.id} has already published a bootcamp.`, 400));
+        }
+        
         const bootcamp = await Bootcamp.create(req.body);
         res.status(201).json({ success: true, data: bootcamp});
     }
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -99,6 +99,11 @@ const BootcampSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
+    }, 
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User', // reference to the User model
+        required: true
     }
 },{
     toJSON: { virtuals: true },
