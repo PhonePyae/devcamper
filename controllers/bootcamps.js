50682a46834f6958ff1dab08974d53798aff687b
Phone Pyae <phonepyaethantzin2000@gmail.com>
2025-03-08 11:59:11 +0800

    Bootcamp Ownership

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -41,7 +41,7 @@ exports.createBootcamp = asyncHandler(
         if(publishedBootcamp && req.user.role !== 'admin'){
             return next(new ErrorResponse(`User ID ${req.user.id} has already published a bootcamp.`, 400));
         }
-        
+
         const bootcamp = await Bootcamp.create(req.body);
         res.status(201).json({ success: true, data: bootcamp});
     }
@@ -52,13 +52,21 @@ exports.createBootcamp = asyncHandler(
 //@access   Private
 exports.updateBootcamp = asyncHandler(
     async (req, res, next) => {
-        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        });
+        let bootcamp = await Bootcamp.findById(req.params.id);
         if(!bootcamp){
             return next (new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
         }
+
+        //Make sure bootcamp
+        if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin'){
+            return next (new ErrorResponse(`User with User ID ${req.params.id} is not authorized to update this bootcamp.`, 401));
+        }
+
+        bootcamp = await Bootcamp.findOneAndUpdate(req.params.id, req.body, {
+            new: true, //Update new content
+            runValidators: true
+        });
+
         res.status(200).json({ success: true, data: bootcamp});
     }
 );
@@ -112,6 +120,11 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id);
     if(!bootcamp){
         return next (new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+    }
+
+    //Make sure bootcamp
+    if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin'){
+        return next (new ErrorResponse(`User with User ID ${req.params.id} is not authorized to update this bootcamp.`, 401));
     }
 
     if(!req.files){
